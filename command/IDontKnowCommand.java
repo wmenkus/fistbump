8ea69e302b8ad76be96b709811c8474557eb5ee3
chdelk <70281412+chdelk@users.noreply.github.com>
2021-10-13 12:42:20 -0400

    Adding files to the Command Design Pattern

--- command/IDontKnowCommand.java
+++ command/IDontKnowCommand.java
@@ -1,9 +1,21 @@
+/**
+ * The command that sings "I Don't Know"
+ * @author: Cameron Delk
+ */
 public class IDontKnowCommand implements Command{
     private MilitaryCadence cadence;
     
+    /**
+     * The parameterized constructor for IDontKnowCommand.
+     * @param cadence is set equal to this.cadence.
+     */
     public IDontKnowCommand(MilitaryCadence cadence) {
         this.cadence = cadence;
     }
+    /**
+     * An implementation of Command's execute method.
+     * calls the singIDontKnow method via the cadence variable.
+     */
     public void execute() {
         cadence.singIDontKnow();
     }
