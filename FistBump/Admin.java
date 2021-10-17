b295f6b025c9b1b5dc0ad8e8846bfad61c1aeb14
unknown <briandw@email.sc.edu>
2021-10-17 03:49:13 -0400

    add account class

---
+++ FistBump/Admin.java
@@ -0,0 +1,28 @@
+package FistBump;
+
+public class Admin extends Account{
+    
+    public Admin(String name, String email, String password){
+
+    }
+
+    public void removeAccount(Account account){
+
+    }
+
+    public void hideRating(Rating rating){
+
+    }
+
+    public void reviseInternship(Internship old, Internship newInternship){
+
+    }
+
+    public void removeInternship(Internship internship){
+
+    }
+
+    public void createAdminAccount(String name, String email, String password){
+
+    }
+}
