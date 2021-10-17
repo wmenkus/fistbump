b1d1be1d35043000cbbccbf111ab1918f54244fe
Walker Menkus <wmenkus1@gmail.com>
2021-10-17 16:39:41 -0400

    Added DataWriter stub and Student stub

---
+++ FistBump/DataWriter.java
@@ -0,0 +1,33 @@
+/**
+ * @author Walker Menkus
+ */
+
+package FistBump;
+
+public class DataWriter {
+    /**
+     * There may be some stuff missing here for the final implementation,
+     * I don't really know how a data writer should work, whether or not there should
+     * be a scanner etc. Refer to the TriviaGame assignment and @portiaportia's DataWriter
+     * for implementation guidance
+     */
+
+
+    //Not all of these may be necessary, I figure we need one method per JSON file we're writing to
+    public void saveAccounts() {
+        //TODO
+    }
+
+    public void saveResumes() {
+        //TODO
+    }
+
+    public void saveInternships() {
+        //TODO
+    }
+
+    public void saveRatings() {
+        //TODO this one especially may not be necessary if we keep ratings contained within the
+        //Account's JSON file somehow
+    }
+}
---
+++ FistBump/Student.java
@@ -0,0 +1,32 @@
+/**
+ * @author Walker Menkus
+ */
+
+package FistBump;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Student extends Account {
+    private double rating;
+    private ArrayList<UUID> resumes; //TODO discuss with team how we will implement UUIDs
+    private int ratingTotal;
+
+    public Student(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password; //TODO fields are not visible, do they need to be protected in Account?
+    }
+
+    public void calcRating(ArrayList<Rating> ratings) {
+        //TODO average the ratings
+    }
+
+    public void createResume() { //Removed Student parameter that was listed on the UML
+        //TODO construct a Resume, I'm not sure if the UI stuff will go here or somewhere else?
+    }
+
+    public void apply(Internship internship) {
+        //TODO adds self to the list of Students on that internship
+    }
+}
