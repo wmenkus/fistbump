8080b69fa8ece15528c23dd7c24693b20da4a737
chdelk <70281412+chdelk@users.noreply.github.com>
2021-10-22 17:08:12 -0400

    Fixing Rating class

--- FistBump/Rating.java
+++ FistBump/Rating.java
@@ -8,64 +8,32 @@ public class Rating {
     private Account rated;
     private boolean valid;
 
-<<<<<<< HEAD
     public Rating() {
-        id = "";
-        rating = 0;
-        raterID = 0;
-        ratedID = 0;
+        rating = 0.0;
+        rater = new Account();
+        rated = new Account();
         valid = true;
-=======
-    public Rating(double rating, Account rater, Account rated) {
+    }
+    public Rating(double rating, Account rater, Account rated, boolean valid) {
+        this.rating = rating;
         this.rater = rater;
         this.rated = rated;
-        this.rating = rating;
+        this.valid = valid;
     }
-
     public double getRating() {
         return this.rating;
     }
-
-    public boolean isValid() {
-        return this.valid;
+    public Account getRater() {
+        return this.rater;
     }
-
-    public void setValid(boolean valid){
-        this.valid = valid;
->>>>>>> 86297744fb9e6bc8151e69693b409e963cc25f06
+    public void setRater(Account rater) {
+        this.rater = rater;
     }
-    public Rating(String id, int rating, int raterID, int ratedID, boolean valid, Account rater, Account rated) {
-        this.id = id;
-        this.rating = rating;
-        this.raterID = raterID;
-        this.ratedID = ratedID;
-        this.valid = valid;
-        rater = new Account();
-        rated = new Account();
+    public Account getRated() {
+        return this.rated;
     }
-    public String getID() {
-        return this.id;
-    }
-    public void setID(String id) {
-        this.id = id;
-    }
-    public int getRating() {
-        return this.rating;
-    }
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-    public int getRaterID() {
-        return this.raterID;
-    }
-    public void setRaterID(int raterID) {
-        this.raterID = raterID;
-    }
-    public int getRatedID() {
-        return this.ratedID;
-    }
-    public void setRatedID(int ratedID) {
-        this.ratedID = ratedID;
+    public void setRated(Account rated) {
+        this.rated = rated;
     }
     public boolean getValid() {
         return this.valid;
