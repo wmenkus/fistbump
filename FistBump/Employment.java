48a30238493c1799705b802d69c647a11fd11367
chdelk <70281412+chdelk@users.noreply.github.com>
2021-10-22 13:39:20 -0400

    Updating files for Final Project

--- FistBump/Employment.java
+++ FistBump/Employment.java
@@ -12,6 +12,47 @@ public class Employment {
     private String contactInfo;
 
     public Employment(){
-        
+        jobTitle = "";
+        companyName = "";
+        startDate = "";
+        endDate = "";
+        contactInfo = "";
+    }
+    public Employment(String jobTitle, String companyName, String startDate, String endDate, String contactInfo) {
+        this.jobTitle = jobTitle;
+        this.companyName = companyName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.contactInfo = contactInfo;
+    }
+    public String getJobTitle() {
+        return this.jobTitle;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+    public String getCompanyName() {
+        return this.companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public String setStartDate() {
+        return this.startDate;
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+    public String getEndDate() {
+        return this.endDate;
+    }
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+    public String getContactInfo() {
+        return this.contactInfo;
+    }
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
     }
 }
--- FistBump/Rating.java
+++ FistBump/Rating.java
@@ -7,7 +7,50 @@ public class Rating {
     private int ratedID;
     private boolean valid;
 
-    public Rating(int rating, Account rater) {
-        
+    public Rating() {
+        id = "";
+        rating = 0;
+        raterID = 0;
+        ratedID = 0;
+        valid = true;
+    }
+    public Rating(String id, int rating, int raterID, int ratedID, boolean valid, Account rater, Account rated) {
+        this.id = id;
+        this.rating = rating;
+        this.raterID = raterID;
+        this.ratedID = ratedID;
+        this.valid = valid;
+        rater = new Account();
+        rated = new Account();
+    }
+    public String getID() {
+        return this.id;
+    }
+    public void setID(String id) {
+        this.id = id;
+    }
+    public int getRating() {
+        return this.rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public int getRaterID() {
+        return this.raterID;
+    }
+    public void setRaterID(int raterID) {
+        this.raterID = raterID;
+    }
+    public int getRatedID() {
+        return this.ratedID;
+    }
+    public void setRatedID(int ratedID) {
+        this.ratedID = ratedID;
+    }
+    public boolean getValid() {
+        return this.valid;
+    }
+    public void setValid(boolean valid) {
+        this.valid = valid;
     }
 }
